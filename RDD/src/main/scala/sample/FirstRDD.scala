3a30703522e96bab97624ebe0c67015cf221913b
kamath7 <adithyakamath96@gmail.com>
2023-01-14 16:58:50 +0530

    result achieved. ratings -> occurence

--- RDD/src/main/scala/sample/FirstRDD.scala
+++ RDD/src/main/scala/sample/FirstRDD.scala
@@ -12,5 +12,21 @@ object FirstRDD {
     val sc = new SparkContext("local[*]", "Ratings")
 
     val lines = sc.textFile("D:\\Code\\Scala\\SparkAndScala\\Datasets\\ml-100k\\u.data")
+
+
+    //This is how the data looks
+
+    /*
+    * userID movieID rating epoch_time
+    * 19 22 3 183818
+    * 23 21 5 919292
+    * */
+
+    val ratings = lines.map(x => x.toString().split("\t")(2)) //converting each line of rating to string
+
+    val results = ratings.countByValue() // gives you 3,2 5,3 . Basically how many times a particular rating ahs appeared
+
+    val sortedRes = results.toSeq.sortBy(_._1)
+    sortedRes.foreach(println)
   }
 }
