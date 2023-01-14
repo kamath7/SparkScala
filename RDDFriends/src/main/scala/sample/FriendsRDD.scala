8ea0a50d3bb2b6c1349ae997e1236cec11deb07a
kamath7 <adithyakamath96@gmail.com>
2023-01-14 17:44:03 +0530

    RDD Friends

---
+++ RDDFriends/src/main/scala/sample/FriendsRDD.scala
@@ -0,0 +1,35 @@
+package sample
+
+import org.apache.log4j._
+import org.apache.spark._
+object FriendsRDD {
+
+  //The dataset will be looking as follows
+
+  //ID, NAME, AGE, NUM_OF_FRIENDS
+  /*
+  1, Adithya, 26, 1
+  2, ShreeHari, 34, 299
+   */
+
+  def parseMyLines(line: String): (Int, Int) = {
+    val fields = line.split(",")
+
+    val age = fields(2).toInt
+    val numOfFriends = fields(3).toInt
+
+    (age, numOfFriends)
+  }
+
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "FriendsRDD")
+
+    val lines = sc.textFile("D:\\Code\\Scala\\SparkAndScala\\Datasets\\fakefriends-noheader.csv")
+
+    val rdd = lines.map(parseMyLines)
+  }
+
+
+}
