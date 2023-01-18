1e67f848794e5545411c9bff8561da8435b2d2f9
kamath7 <adithyakamath96@gmail.com>
2023-01-18 19:14:23 +0530

    small bug

--- AdvancedSpark-Movies/src/main/scala/sample/ABetterMoviesOne.scala
+++ AdvancedSpark-Movies/src/main/scala/sample/ABetterMoviesOne.scala
@@ -27,4 +27,36 @@ object ABetterMoviesOne {
 
     movieNames
   }
+
+  def main(args: Array[String]) = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder.appName("PopMovies").master("local[*]").getOrCreate()
+
+    val nameDict = spark.sparkContext.broadcast(loadMovieNames())
+
+    val movieSchema = new StructType()
+      .add("userID", IntegerType, nullable = true)
+      .add("movieID", IntegerType, nullable = true)
+      .add("rating", IntegerType, nullable = true)
+      .add("timestamp", LongType, nullable = true)
+
+    import spark.implicits._
+    val moviesDB = spark.read.option("sep","\t").schema(movieSchema).csv("D:\\Code\\Scala\\SparkAndScala\\Datasets\\ml-100k\\u.data").as[Movies]
+
+    val topMovieIds = moviesDB.groupBy("movieID").count()
+
+    val lookUpName: Int => String = (movieID: Int) => {
+      nameDict.value(movieID)
+    }
+
+    val lookUpNameUDF = udf(lookUpName)
+
+    val movieWithNames = topMovieIds.withColumn("MovieTitle",lookUpNameUDF(col("movieID")))
+
+    val sortedMovieNames = movieWithNames.sort("count")
+
+    sortedMovieNames.show(sortedMovieNames.count.toInt, truncate = false)
+  }
 }
