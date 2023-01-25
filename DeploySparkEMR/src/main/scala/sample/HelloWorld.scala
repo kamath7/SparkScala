dce3b56d6f63b9ff9813bc2ffd2f081b1765a26c
kamath7 <adithyakamath96@gmail.com>
2023-01-25 19:10:43 +0530

    Creating twitter streams

--- DeploySparkEMR/src/main/scala/sample/HelloWorld.scala
+++ DeploySparkEMR/src/main/scala/sample/HelloWorld.scala
@@ -1,5 +1,20 @@
 package sample
 
+import org.apache.log4j._
+import org.apache.spark._
+
 object HelloWorld {
 
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "HelloThere")
+
+    val lines = sc.textFile("data/u.data")
+
+    val numLines = lines.count()
+
+    println("The data file you entered has "+numLines+" lines")
+    sc.stop()
+  }
 }
---
+++ SparkStreaming/src/main/scala/sample/HashTagListener.scala
@@ -0,0 +1,14 @@
+package sample
+
+import org.apache.log4j._
+
+object HashTagListener {
+
+  def setupLogger(): Unit = {
+    val rootLogger = Logger.getRootLogger
+    rootLogger.setLevel(Level.ERROR)
+  }
+  def setUpTwitter():Unit = {
+    import scala
+  }
+}
